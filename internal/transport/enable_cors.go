a5374a8b846be1168a1bbb552eeda7c96800c9d6
tytapory <bezmaternih.vladislav@yandex.ru>
2025-05-18 12:16:10 +0300

    Fix: cors error

--- internal/transport/enable_cors.go
+++ internal/transport/enable_cors.go
@@ -4,10 +4,12 @@ import "net/http"
 
 func enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
 			return
 		}
 		next.ServeHTTP(w, r)
